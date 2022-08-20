a079c97c60613559ffd61e1f247cebd58f1d8d79
Aurelien Massiot <aurelien.massiot@octo.com>
2022-08-20 23:13:02 +0200

    add search address

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # hackathon_centrale
 Hackathon à Centrale Lyon en Septembre 2022.
 
+## Comment faire fonctionner ?
+1. Installer les dépendances du `requirements.txt`
+2. Générer un token [Mapbox](https://docs.mapbox.com/help/getting-started/access-tokens/) et l'enregistrer sous forme de fichier `.mapbox_token` à la racine du projet.
+
 ## Données utilisées 
 Les données utilisées proviennent du projet [ufo-reports](https://github.com/planetsig/ufo-reports/blob/master/csv-data/ufo-complete-geocoded-time-standardized.csv).
+
--- dashboard.py
+++ dashboard.py
@@ -1,31 +1,37 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+from geopy.geocoders import Nominatim
 
 from feature_engineering import load_data, clean_data, filter_by_year
 
+# Configuration
+mapbox_access_token = open(".mapbox_token").read()
 st.set_page_config(
     page_title="Explorateur d'OVNIs",
     page_icon='./images/logo_ovni.jpeg',
     layout='wide',
     initial_sidebar_state='auto',
 )
+geolocator = Nominatim(user_agent="dashboard")
 st.title("Explorateur d'OVNIs")
 
 # Loading
 df = load_data()
 df = clean_data(df)
 
+# Sidebar
 selected_year_min, selected_year_max = st.sidebar.slider(
     label='Dates',
     min_value=int(df['event_date_year'].min()),
     max_value=int(df['event_date_year'].max()),
     value=(1950, 2000)
 )
-
 df_filtered = filter_by_year(df, selected_year_min, selected_year_max)
+searched_address = st.sidebar.text_input(label='Chercher une adresse', value='New York')
+location = geolocator.geocode(query=searched_address, country_codes='us')
 
-# # Feature Engineering
+# Feature Engineering
 df_grouped_by_accident_severity_and_year = pd.DataFrame(df_filtered.groupby(by=['event_date_year', 'shape']).size(),
                                                         columns=['count']).reset_index()
 df_grouped_by_accident_severity_and_dayname = pd.DataFrame(
@@ -38,26 +44,26 @@ st.write(df)
 
 ## Map
 st.subheader('Carte')
-latitude = 40
-longitude = -95
-map_graph = px.scatter_mapbox(
-    df_filtered,
-    lat='latitude',
-    lon='longitude',
-    color='shape',
-    hover_data={'latitude': True,
-                'longitude': True,
-                'event_date': True,
-                'shape': True
-                },
-    color_continuous_scale=px.colors.sequential.Blackbody_r,
-    size_max=15,
-    zoom=3,
-    opacity=1,
-    center={'lat': latitude, 'lon': longitude},
-    mapbox_style='carto-positron',
-)
-st.plotly_chart(map_graph, use_container_width=True)
+if searched_address:
+    st.sidebar.success(location.address)
+    map_graph = px.scatter_mapbox(
+        df_filtered,
+        lat='latitude',
+        lon='longitude',
+        color='shape',
+        hover_data={'latitude': True,
+                    'longitude': True,
+                    'event_date': True,
+                    'shape': True
+                    },
+        color_continuous_scale=px.colors.sequential.Blackbody_r,
+        size_max=15,
+        zoom=8,
+        opacity=1,
+        center={'lat': location.latitude, 'lon': location.longitude},
+        mapbox_style='carto-positron',
+    )
+    st.plotly_chart(map_graph, use_container_width=True)
 
 ## Line graph
 st.subheader("Évolution du nombre d'OVNIs par année")
